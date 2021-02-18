e230a812939f751030095d1082b61984bc898b6f
Nick Berliner <nick.berliner96@gmail.com>
2021-02-18 21:08:57 +0100

    navbar created

---
+++ components/MyNavbar.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+    
+    <!-- <b-navbar type="dark" variant="dark">
+        <b-navbar-nav class="navbar"> -->
+    <nav class="navbar">
+            <nuxt-link to="/" class="link">Home</nuxt-link>
+            <nuxt-link to="/" class="link">About</nuxt-link>
+            <nuxt-link to="/" class="link">Events</nuxt-link>
+            <nuxt-link to="/" class="link">Create Event</nuxt-link>
+    </nav>
+        <!-- </b-navbar-nav>
+    </b-navbar> -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyNavbar'
+}
+</script>
+
+<style scoped>
+/* .navbar {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    padding: 1.7rem;
+}
+
+.link {
+    margin-right: 5rem;
+}
+
+.link:hover {
+    color: green;
+    text-decoration: none;
+} */
+</style>
